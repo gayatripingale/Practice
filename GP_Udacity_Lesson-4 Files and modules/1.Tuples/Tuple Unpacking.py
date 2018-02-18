e8a9fe250f5678b288e44e32e0a648892631e7e2
Gayatri Pingale <gayatripingale@MacBook.local>
2018-02-18 15:58:55 -0600

    fixing

--- GP_Udacity_Lesson-4 Files and modules/1.Tuples/Tuple Unpacking.py
+++
@@ -1,6 +0,0 @@
-"""
-- Tuples can be used to assign the multiple variables in compact way.
-"""
-dimentions = 52,40,100
-length,width,height = dimentions
-print( "\n\nThe dimentions are {} x {} x {}".format(length,width,height))
